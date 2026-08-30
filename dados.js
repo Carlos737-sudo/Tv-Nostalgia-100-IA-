6e9eb99fbc27fdc3d72ac1400444ae33f4443ce7
Carlos737-sudo <carloshenrique272009@gmail.com>
2026-08-30 15:47:48 -0300

    Update character list in dados.js
    Replaced the existing character list with new characters from various shows, including their details like id, name, category, period, initials, color, votes, and photo URL.

--- dados.js
+++ dados.js
@@ -1,14 +1,8 @@
-// dados.js
-// Lista de personagens exibidos nas telas de Fotos, Votar e Placar.
-// "iniciais" e "cor" são usados para gerar um avatar caso não haja foto (campo "foto").
-
 const PERSONAGENS = [
-    { id: "chaves", nome: "Chaves", categoria: "Chaves", periodo: "Anos 80", iniciais: "CH", cor: "#7a5230", votos: 1},
-    { id: "malu", nome: "Malu", categoria: "Anos Dourados", periodo: "Anos 70", iniciais: "MA", cor: "#5c2a3a", votos: 9 },
-    { id: "zeca", nome: "Zeca", categoria: "A Grande Família", periodo: "Anos 2000", iniciais: "ZE", cor: "#2a4a5c", votos: 6 },
-    { id: "tia-nastacia", nome: "Tia Nastácia", categoria: "Sítio do Picapau Amarelo", periodo: "Anos 70", iniciais: "TN", cor: "#5c4326", votos: 3 },
-    { id: "magali", nome: "Magali", categoria: "Turma da Mônica", periodo: "Anos 80", iniciais: "MG", cor: "#7a2f2f", votos: 5 },
-    { id: "cascao", nome: "Cascão", categoria: "Turma da Mônica", periodo: "Anos 80", iniciais: "CA", cor: "#3f5c2a", votos: 0 },
-    { id: "trapalhao", nome: "Trapalhão", categoria: "Os Trapalhões", periodo: "Anos 80", iniciais: "TR", cor: "#5c3a26", votos: 30 },
-    { id: "helena", nome: "Helena", categoria: "Laços de Família", periodo: "Anos 2000", iniciais: "HE", cor: "#3a2a5c", votos: 210 },
-];
+    { id: "Irmão do Jorel", nome: "Irmão do Jorel", categoria: "Irmão do Jorel", periodo: "Ano 2014", iniciais: "IDJ", cor: "#7a5230", votos: 130, foto: "http://irmaodojorel.com.br/wp-content/uploads/2023/08/01-Irmao-do-Jorel.webp"},
+    { id: "Júlio", nome: "Júlio", categoria: "Cocoricó", periodo: "Ano 1996", iniciais: "Ccrco", cor: "#5c2a3a", votos: 9, foto: ""},
+    { id: "Marina", nome: "Marina", categoria: "Peixonauta", periodo: "Ano 2009", iniciais: "PXT", cor: "#2a4a5c", votos: 6, foto: ""},
+    { id: "Layla Bach", nome: "Layla Bach", categoria: "UNDER UNDERGROUNDS", periodo: "Ano 2016", iniciais: "TUU", cor: "#5c4326", votos: 3, foto: ""},
+    { id: "Tom", nome: "Tom", categoria: "Detetives do Prédio Azul", periodo: "Ano 2012", iniciais: "DPA", cor: "#7a2f2f", votos: 5, foto: ""},
+    { id: "Luna", nome: "Luna", categoria: "O Show da Luna", periodo: "Ano 2014", iniciais: "OSDL", cor: "#3f5c2a", votos: 0, foto: ""},
+] 
